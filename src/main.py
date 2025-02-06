01fad25f8468da65f877b0b0055f33b334daf6bc
sqrtminustwo <intmain235@gmail.com>
2025-02-06 04:31:53 +0100

    Drawing resolution based.

--- src/main.py
+++ src/main.py
@@ -1,52 +1,54 @@
 import pygame 
 
+#constants
 background_colour = (0, 0, 0) 
 WHITE = (255, 255, 255)
-placed = [0] * 9
 
 #main game
+width, height = 900, 1050
+margin = 20
 placed = [0] * 9
 
 coords1 = [0] * 9
-y, index = 133, 0
+y, index = (height/3)/2, 0
 for i in range(3):
-    x = 133
+    x = (width/3)/2
     for j in range(3):
         coords1[index] = [x,y]
-        x += 133*2
+        x += ((width/3)/2)*2
         index += 1
-    y += 133*2
+    y += ((height/3)/2)*2
 
 coords2= [0] * 9
-y, w, index = 20, 246, 0
+y, w, index = margin, (height/3)-margin, 0
 for i in range(3):
-    x, z = 20, 246
+    x, z = margin, (width/3)-margin
     for j in range(3):
         coords2[index] = [[[x, y], [z, w]], [[z, y], [x, w]]]
-        x += 266
-        z += 266
+        x += (width/3)
+        z += (width/3)
         index += 1
-    y += 266
-    w += 266
+    y += (height/3)
+    w += (height/3)
 
 limits = [0] * 9
 z, index = 0, 0
 for i in range(3):
     x = 0
     for j in range(3):
-        limits[index] = [[x, x + 266], [z, z + 266]]
-        x += 266
+        limits[index] = [[x, x + (height/3)], [z, z + (height/3)]]
+        x += (width/3)
         index += 1
-    z += 266
+    z += (height/3)
 
-screen = pygame.display.set_mode((800, 800)) 
+screen = pygame.display.set_mode((width, height)) 
 pygame.display.set_caption('Fortnite') 
 screen.fill(background_colour) 
 
-pygame.draw.line(screen, WHITE, (266,0), (266,800))
-pygame.draw.line(screen, WHITE, (532,0), (532,800))
-pygame.draw.line(screen, WHITE, (0,266), (800,266))
-pygame.draw.line(screen, WHITE, (0,532), (800,532))
+pygame.draw.line(screen, WHITE, (width/3,0), (width/3,height))
+pygame.draw.line(screen, WHITE, ((width/3)*2,0), ((width/3)*2,height))
+pygame.draw.line(screen, WHITE, (0,height/3), (width,height/3))
+pygame.draw.line(screen, WHITE, (0,(height/3)*2), (width,(height/3)*2))
 
 pygame.display.flip() 
 
@@ -60,7 +62,7 @@ def drawCircle(figure):
     for i in range(len(limits)):
         if (limits[i][0][0] < pos[0] < limits[i][0][1]) and (limits[i][1][0] < pos[1] < limits[i][1][1]) and (placed[i] == 0):
             if figure:
-                pygame.draw.circle(screen, WHITE, coords1[i], 125, width=1)
+                pygame.draw.circle(screen, WHITE, coords1[i], (width/3-2*margin)/2, width=1)
                 placed[i] = 1;
             else:
                 pygame.draw.line(screen, WHITE, coords2[i][0][0], coords2[i][0][1])
